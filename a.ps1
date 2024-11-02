780df319363ca72c2367b35349ffe5abccff52f0
ogatatsu <ogwrtty@gmail.com>
2024-11-02 15:31:58 +0900

    a

--- a.ps1
+++ a.ps1
@@ -2,5 +2,5 @@ cd $PSScriptRoot
 wget "https://www.nicovideo.jp/ranking/genre/all?term=24h&rss=2.0&lang=ja-jp" -OutFile "all.txt"
 wget "https://www.nicovideo.jp/ranking/genre/music_sound?term=24h&rss=2.0&lang=ja-jp" -OutFile "music_sound.txt"
 git add .
-git commit -a -m "a"
+git commit -m "a"
 git push -f
